951f3a5636866fee43764d2ae9f9f356710c0dca
seven <lvbingzz@qq.com>
2017-12-19 13:51:55 +0800

    user表 注解事物开启

--- src/main/java/com/yunxi/map/service/mapUserService.java
+++ src/main/java/com/yunxi/map/service/mapUserService.java
@@ -4,6 +4,7 @@ import com.yunxi.map.dao.MapUserMapper;
 import com.yunxi.map.entity.mapuser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by MyPC on 2017/12/19.
@@ -22,15 +23,17 @@ public class mapUserService {
     public mapuser getUserByid(int id){
         return mapUserMapper.getUserByid(id);
     }
-
+    @Transactional
     public void registeredoradduser(mapuser user){
         mapUserMapper.registeredoradduser(user);
     }
-
+    @Transactional
     public void deluser(int id){
         mapUserMapper.deluser(id);
     }
+    @Transactional
     public void updateuser(mapuser user){
+
         mapUserMapper.updateuser(user);
     }
 
